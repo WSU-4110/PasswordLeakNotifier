1be2edd5caa118070e2ea7645cf2a15b4ca6d9fd
AlaaA91 <aahmed9104@gmail.com>
2025-06-05 14:41:55 -0400

    Created js file

---
+++ script.js
@@ -0,0 +1,10 @@
+// After valid email input
+
+document.getElementById("emailForm").addEventListener("submit", function (){
+
+    const emailInput = document.getElementById("emailInput");
+    if (emailInput.checkValidity()){
+        
+    }
+
+})
