f43a97fb83a7d8cef1084a2f617f879bc9c7d777
AlaaA91 <aahmed9104@gmail.com>
2025-06-05 15:01:21 -0400

    Started working on results div

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 // After valid email input
 
-document.getElementById("emailForm").addEventListener("submit", function (){
+document.getElementById("emailForm").addEventListener("submit", function (event){
+    event.preventDefault();
 
     const emailInput = document.getElementById("emailInput");
+    const results = document.getElementById("results");
+
     if (emailInput.checkValidity()){
-        
+        results.classList.remove("d-none");
+
+    } else {
+        console.log("Invalid email.")
     }
 
 })
