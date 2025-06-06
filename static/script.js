986f4b7273a8389d1e16d520b8508e9482edc639
AlaaA91 <aahmed9104@gmail.com>
2025-06-05 21:26:10 -0400

    script.js now scrolls results div into view when it appears, and fixed error where an empty input was considered a valid email

--- static/script.js
+++ static/script.js
@@ -4,10 +4,12 @@ document.getElementById("emailForm").addEventListener("submit", function (event)
     event.preventDefault();
 
     const emailInput = document.getElementById("emailInput");
+    const emailValue = emailInput.value.trim();
     const results = document.getElementById("results");
 
-    if (emailInput.checkValidity()){
+    if (emailInput.checkValidity() && emailValue !== ""){
         results.classList.remove("d-none");
+        results.scrollIntoView({ behavior: 'smooth' });
 
     } else {
         console.log("Invalid email.")
