1e3fac3fb58b347122d6076884c3e70c592791fd
AlaaA91 <aahmed9104@gmail.com>
2025-06-05 22:35:20 -0400

    Added reminders prompt with radio buttons

--- static/script.js
+++ static/script.js
@@ -6,10 +6,13 @@ document.getElementById("emailForm").addEventListener("submit", function (event)
     const emailInput = document.getElementById("emailInput");
     const emailValue = emailInput.value.trim();
     const results = document.getElementById("results");
+    const reminders = document.getElementById("reminders");
 
     if (emailInput.checkValidity() && emailValue !== ""){
         results.classList.remove("d-none");
         results.scrollIntoView({ behavior: 'smooth' });
+        reminders.classList.remove("d-none");
+        
 
     } else {
         console.log("Invalid email.")
